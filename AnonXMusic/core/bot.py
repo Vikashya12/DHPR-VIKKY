9dd82b1dfb4fcf07a68af2dfca5ba6bc80243dc7
Vikky yadav <146602351+Vikashya12@users.noreply.github.com>
2024-06-10 05:28:04 +0530

    Update bot.py
    Signed-off-by: Vikky yadav  <146602351+Vikashya12@users.noreply.github.com>

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class Anony(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"𝗦ᴛᴀʀᴛɪɴɢ 𝗕ᴏᴛ...")
         super().__init__(
             name="AnonXMusic",
             api_id=config.API_ID,
